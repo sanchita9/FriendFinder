45e64566df878879d88c89acb71ce7a43cd8fdf7
sanchita9 <sanchita9@gmail.com>
2017-10-19 21:49:43 -0700

    Fully functioning app

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    "name": 'Sanchita Mohite',
+    "profilePic": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDebUCEzotOUyE747aap9m2TY3A9_1GWwdLDtK_2DowN-gcSb9Wg',
+    "scores": [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
---
+++ server.js
@@ -0,0 +1,25 @@
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+// var PORT = 3000;
+app.set('port', (process.env.PORT || 3000));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//ROUTER
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// Starts the server to begin listening
+app.listen(app.get('port'), function() {
+  console.log("App listening on PORT " + app.get('port'));
+});
